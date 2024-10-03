a042382a7978b87004e0dd55f9aebe80e6ca246c
JULIANO TAVARES DA SILVA <juliano_t_da-silva@estudante.sesisenai.org.br>
2024-10-03 10:17:39 -0300

    camera commit

---
+++ app/Camera/index.jsx
@@ -0,0 +1,55 @@
+import { useState, useRef } from "react";
+import { View, StyleSheet, Text, Image, Button } from "react-native";
+import { CameraView, useCameraPermissions } from "expo-camera";
+
+export default function Camera() {
+    const [permissao, pedirPermissao] = useCameraPermissions()
+    const [foto, setFoto] = useState(null)
+    const cameraRef = useRef(null)
+
+    if(!permissao) {
+        return <View></View>
+    }
+
+    if (!permissao.granted){
+        return(
+            <View style={styles.container}>
+                <Text style={styles.textopermissao}>Você precisa permitir o aplicativo acessar sua camera</Text>
+                <Button
+                title='pedir permissão'
+                onPress={pedirPermissao}
+                />
+            </View>
+        )
+    }
+
+    const tirarFoto = async () => {
+        const foto = await cameraRef.current?.takePictureAsync({
+            quality: 0.5,
+            base64: true
+        })
+        setFoto(foto)
+        console.log(foto)   
+    }
+
+    return(
+        <CameraView style={styles.camera} facing={'back'} ref={cameraRef}>
+            <View style={styles.buttonContainer}>
+                <Button title='tirar foto' onPress={tirarFoto}/>
+            </View>    
+        </CameraView>
+    )
+}
+
+const styles = StyleSheet.create({
+container:{
+    flex: 1,
+    justifyContent: 'center',
+},
+textopermissao:{
+    textAlign: 'center',
+},
+camera:{
+
+},
+})
